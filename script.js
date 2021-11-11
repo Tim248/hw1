91a2443d0c27396451cd240df275304a49ebd32f
T1mm <45312243+Tim248@users.noreply.github.com>
2021-11-11 16:23:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 var UserAge = +prompt('Введите свой возраст : ');
 
-if (UserAge <= 18) {
-    alert('Вы еще молоды. Вам нужно учится !');
-}
-else if (UserAge <= 50) {
+(UserAge <= 18) ?
+    alert('Вы еще молоды. Вам нужно учится !')
+:
+else if (UserAge <= 50) 
     alert('Вам нужно работать !');
-}
-else if (UserAge <= 59) {
+:
+else if (UserAge <= 59) 
     alert('Вам скоро на пенсию !');
-}
-else if (UserAge <= 150) {
+:
+else if (UserAge <= 150) 
     alert('Вы пенсионер !');
-}
-else {
+:
+else 
     alert('Поздравляем вы не человек !)');
-}
